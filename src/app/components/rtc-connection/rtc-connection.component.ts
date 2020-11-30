15e8432205e8f533c7d12d2c35e02dada95ef0ca
Mfron <matthieu@pharma-express.com>
2020-11-30 18:20:20 +0100

    improve parameters lisibility

--- src/app/components/rtc-connection/rtc-connection.component.ts
+++ src/app/components/rtc-connection/rtc-connection.component.ts
@@ -1,43 +1,49 @@
-import { Component, Input, ApplicationRef } from "@angular/core";
+import { Component, Input, ApplicationRef } from '@angular/core';
 import { RTCUserService, User } from 'src/app/services/rtc-user.service';
-import { RTCConnection, RTCReceiver, RTCInitiator } from "light-rtc";
+import { RTCConnection, RTCReceiver, RTCInitiator } from 'light-rtc';
 
 @Component({
-  selector: "app-rtc-connection",
-  templateUrl: "./rtc-connection.component.html",
-  styleUrls: ["./rtc-connection.component.scss"]
+  selector: 'app-rtc-connection',
+  templateUrl: './rtc-connection.component.html',
+  styleUrls: ['./rtc-connection.component.scss']
 })
 export class RTCConnectionComponent {
   @Input()
   public user: User;
-  public mainStream : MediaStream;
-  public usersPeer : UserPeer[] = []
+  public mainStream: MediaStream;
+  public usersPeer: UserPeer[] = [];
 
-  constructor(private rtcUsers : RTCUserService, private appRef : ApplicationRef) {
+  constructor(private rtcUsers: RTCUserService, private appRef: ApplicationRef) {
     navigator.mediaDevices.getUserMedia({video : true})
       .then(stream => {
         this.mainStream = stream;
         this.initRTC();
+
       });
   }
 
-  private initRTC() : void {
+  private initRTC(): void {
     this.rtcUsers.join(this.user).forEach(u => {
       const peer = new RTCReceiver(this.mainStream, (infos) => this.rtcUsers.sendInvitation(this.user, u, infos));
+      peer.onMsg('test', console.log);
+      peer.send('test', 'abc');
       this.addUser(u, peer);
+
     });
     this.rtcUsers.userJoined.subscribe(u => {
-      const peer =  new RTCInitiator(this.mainStream, (infos) => this.rtcUsers.sendInvitation(this.user, u, infos));
+      const peer =  new RTCInitiator(this.mainStream, (infos) => this.rtcUsers.sendInvitation(this.user, u, infos), undefined, undefined, undefined, ['test']);
+      peer.onMsg('test', console.log);
+      peer.send('test', 'abc');
       this.addUser(u, peer);
     });
     this.rtcUsers.onInvitation(this.user, (invitation, user) => this.getPeer(user).addInformations(invitation));
   }
 
-  private getPeer(user : User) : RTCConnection {
-    return this.usersPeer.find(p => p.user == user).peer;
+  private getPeer(user: User): RTCConnection {
+    return this.usersPeer.find(p => p.user === user).peer;
   }
 
-  private addUser(user : User, connection : RTCConnection) : void {
+  private addUser(user: User, connection: RTCConnection): void {
     const userPeer = {
       user,
       peer : connection,
@@ -46,13 +52,13 @@ export class RTCConnectionComponent {
     userPeer.peer.onStream(stream => {
       userPeer.stream = stream;
       this.appRef.tick();
-    })
+    });
     this.usersPeer.push(userPeer);
-  } 
+  }
 }
 
 export class UserPeer {
-  user : User;
-  peer : RTCConnection;
-  stream : MediaStream;
+  user: User;
+  peer: RTCConnection;
+  stream: MediaStream;
 }
